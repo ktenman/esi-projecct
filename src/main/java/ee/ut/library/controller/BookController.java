f54ee55cf94ad04f2da8af46151cf65150ae2359
Siim-Morten Ojasalu <siim.ojasalu@gmail.com>
2022-05-09 00:38:09 +0300

    Added methods to BookController

--- src/main/java/ee/ut/library/controller/BookController.java
+++ src/main/java/ee/ut/library/controller/BookController.java
@@ -1,5 +1,6 @@
 package ee.ut.library.controller;
 
+import ee.ut.library.domain.enums.Status;
 import ee.ut.library.domain.entity.Book;
 import ee.ut.library.service.BookService;
 import io.swagger.annotations.ApiOperation;
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.*;
 import javax.validation.Valid;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @AllArgsConstructor
@@ -52,5 +56,52 @@ public class BookController {
         bookService.deleteById(id);
     }
 
+    @GetMapping("/titles/{title}")
+    @ApiOperation(value = "Retrieves books by title")
+    public List<Book> getBooksByTitle(@PathVariable String title) {
+        List<Book> books = bookService.findAll();
+        // Regex to match the title
+        books.removeIf(book -> !Pattern.matches(title, book.getTitle()));
+        return books;
+    }
 
+    @GetMapping("/authors/{author}")
+    @ApiOperation(value = "Retrieves books by author")
+    public List<Book> getBooksByAuthor(@PathVariable String author) {
+        List<Book> books = bookService.findAll();
+        books.removeIf(book -> !Pattern.matches(author, book.getAuthor()));
+        return books;
+    }
+
+    @GetMapping("/languages/{lang}")
+    @ApiOperation(value = "Retrieves books by language")
+    public List<Book> getBooksByLanguage(@PathVariable String lang) {
+        List<Book> books = bookService.findAll();
+        books.removeIf(book -> !book.getLanguage().equals(lang));
+        return books;
+    }
+
+    @GetMapping("/statuses/{status}")
+    @ApiOperation(value = "Retrieves books by status")
+    public List<Book> getBooksByStatus(@PathVariable String status) {
+        List<Book> books = bookService.findAll();
+        books.removeIf(book -> !book.getStatus().equals(status));
+        return books;
+    }
+
+    @GetMapping("/years/{year}")
+    @ApiOperation(value = "Retrieves books by year")
+    public List<Book> getBooksByYear(@PathVariable String year) {
+        List<Book> books = bookService.findAll();
+        books.removeIf(book -> !book.getReleaseDate().equals(year));
+        return books;
+    }
+
+    @GetMapping("/available")
+    @ApiOperation(value = "Retrieves available books")
+    public List<Book> getAvailableBooks() {
+        List<Book> books = bookService.findAll();
+        books.removeIf(book -> !book.getStatus().equals(Status.AVAILABLE));
+        return books;
+    }
 }
